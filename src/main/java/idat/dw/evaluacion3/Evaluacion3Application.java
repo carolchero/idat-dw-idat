887f654d7190d53e0bc818304e0fd100c31a1388
Carol Chero Paz <cheropazpilar@gmail.com>
2023-07-27 22:50:28 -0500

    ultimos cambios

---
+++ src/main/java/idat/dw/evaluacion3/Evaluacion3Application.java
@@ -0,0 +1,13 @@
+package idat.dw.evaluacion3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class evaluacion33Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Practica03Application.class, args);
+    }
+
+}
